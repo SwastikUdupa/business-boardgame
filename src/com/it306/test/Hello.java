b24ad105c59a0afce7f918496aff5a2dc589a337
Amith Kini <kini.amit.22@gmail.com>
2017-03-11 00:42:32 +0530

    Initial commit

---
+++ src/com/it306/test/Hello.java
@@ -0,0 +1,14 @@
+package com.it306.test;
+/*
+ * This is a test file made in the beginning of the project. This
+ * class file and the package can be safely deleted without any 
+ * dependency issues.
+ * @Author Amith Kini 
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		// This prints out some text in the console.
+		System.out.println("Hello, world!");
+	}
+}
